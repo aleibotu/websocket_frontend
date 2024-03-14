e7cbc35475e49ed18d5bd7006e988504c056f3e2
aleivc <aleivc@outlook.com>
2024-03-14 09:35:51 +0800

    merge dashboard MyChart

--- src/app/dashboard/MyChart.js
+++ src/app/dashboard/MyChart.js
@@ -1,33 +1,70 @@
-'use client';
-import {Line} from "@ant-design/charts";
-import {useEffect, useState} from "react";
+'use client'
+import {MyBar, MyDual, MyGauge, MyLiquid, MyPie, MyRadiaGraph, MyTinyArea} from "./components";
+import {Alert} from "antd";
+
+function Wrap({children, styles}) {
+    // 地图一出现，背景就没了。
+    return (
+        <div style={{
+            position: "relative",
+            width: 350,
+            height: 160,
+            padding: 5,
+            background: 'rgba(80, 196, 237, 0.2)',
+            zIndex: 11,
+            marginTop: 10,
+            borderRadius: 10,
+            ...styles
+        }}>
+            {children}
+        </div>
+    )
+}
 
 export default function MyChart() {
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        asyncFetch();
-    }, []);
-
-    const asyncFetch = () => {
-        fetch('https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json')
-            .then((response) => response.json())
-            .then((json) => setData(json))
-            .catch((error) => {
-                console.log('fetch data failed', error);
-            });
-    };
-    const config = {
-        data,
-        padding: 'auto',
-        xField: 'Date',
-        yField: 'scales',
-        xAxis: {
-            // type: 'timeCat',
-            tickCount: 5,
-        },
-    };
     return (
-        <Line {...config} />
+        <div style={{display: "flex", flexDirection: 'column', margin: '0 10px', height: '100%'}}>
+            <div style={{height: 68, display: 'flex', alignItems: 'center', justifyContent: "space-between"}}>
+                <Wrap styles={{height: 40}}>
+                    <div style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        height: '100%',
+                        fontSize: 24,
+                        fontWeight: 'bold',
+                        letterSpacing: 7,
+                        paddingLeft: 5
+                    }}>5G环境监测系统
+                    </div>
+                </Wrap>
+                <div>
+                    <Alert message="Warning Text" type="warning" closable onClose={() => console.log('close')}
+                           style={{width: 700}}/>
+                </div>
+            </div>
+            <div style={{display: 'flex', flex: 1}}>
+                <div style={{width: 350}}>
+                    <Wrap><MyPie/></Wrap>
+                    <Wrap><MyBar/></Wrap>
+                    <Wrap><MyRadiaGraph/></Wrap>
+                    <Wrap><MyTinyArea/></Wrap>
+                    <Wrap><MyTinyArea/></Wrap>
+                </div>
+                <div style={{display: 'flex', flexDirection: 'column-reverse', flex: 1}}>
+                    <div style={{display: 'flex', justifyContent: 'space-between', paddingLeft: 24 }}>
+                        <Wrap><MyGauge/></Wrap>
+                        <Wrap><MyGauge/></Wrap>
+                        <Wrap><MyGauge/></Wrap>
+                        <Wrap><MyGauge/></Wrap>
+                    </div>
+                    <div style={{alignSelf: 'flex-end'}}>
+                        <Wrap styles={{width: 700}}><MyDual/></Wrap>
+                    </div>
+                    <div style={{alignSelf: 'flex-end'}}>
+                        <Wrap><MyLiquid/></Wrap>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
