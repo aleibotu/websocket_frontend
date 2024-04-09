afc69cfd52427d8a2f41c129385788923ebf1da8
aleivc <aleivc@outlook.com>
2024-04-09 09:27:36 +0800

    remove link for upgrade

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,12 @@
 import Report from "@/app/Report";
-import Link from "next/link";
-import {Button} from "antd";
 
 export default async function Home() {
     return (
         <>
-            <div style={{height: 50, width: 'calc(100% - 20px)', paddingLeft: 10, display: 'flex', alignItems: 'center', justifyContent: 'flex-start', gap: 80}}>
-                <Link href="/dashboard" target="_blank"><Button>大屏</Button></Link>
-                <Link href="/camera" target="_blank"><Button>摄像头</Button></Link>
-            </div>
+            {/*<div style={{height: 50, width: 'calc(100% - 20px)', paddingLeft: 10, display: 'flex', alignItems: 'center', justifyContent: 'flex-start', gap: 80}}>*/}
+            {/*    <Link href="/dashboard" target="_blank"><Button>大屏</Button></Link>*/}
+            {/*    <Link href="/camera" target="_blank"><Button>摄像头</Button></Link>*/}
+            {/*</div>*/}
             <Report />
         </>
     )
